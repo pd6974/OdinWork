0984be0dd852c67adb5eb52be52e2c4481d494e3
pd6974 <pdolan17@gmail.com>
2022-10-22 13:58:54 -0500

    Creates base files with function to create divs onload

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,12 @@
+//variables to help me make the grid
+const container = document.querySelector('.container');
+
+
+//function to build grid onload
+function makeGrid() {
+    for (let i = 0; i < 15; i++) {
+        const gridDiv = document.createElement('div');
+        container.appendChild(gridDiv);
+        gridDiv.classList.add('gridDiv');
+    }
+}
