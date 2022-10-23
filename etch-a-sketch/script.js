6dde29e3bad24adc973da92aeacf015b837af256
pd6974 <pdolan17@gmail.com>
2022-10-22 21:30:37 -0500

    Creates 16x16 grid of squares through js and css tools

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -4,9 +4,9 @@ const container = document.querySelector('.container');
 
 //function to build grid onload
 function makeGrid() {
-    for (let i = 0; i < 15; i++) {
-        const gridDiv = document.createElement('div');
-        container.appendChild(gridDiv);
-        gridDiv.classList.add('gridDiv');
+    for (let i = 0; i < 256; i++) {
+            const gridDiv = document.createElement('div');
+            container.appendChild(gridDiv);
+            gridDiv.classList.add('gridDiv');
     }
 }
